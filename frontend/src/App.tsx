9d3593d25f9620f410a75d35c49a082dc5745f60
KASHISH KUMAWAT <ms.kashishkmt63@gmail.com>
2025-03-10 17:34:39 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      
+        <div className = "bg-red-600"> hii there </div>
+    </>
+  )
+}
+
+export default App
